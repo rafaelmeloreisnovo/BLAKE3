1b8238f529129f0fab66383c39d4b499d7be60a2
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-01-22 18:53:32 -0300

    Update pai_sign.h

--- rmr/pai_sign.h
+++ rmr/pai_sign.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024–2026 Rafael Melo Reis
+ * Licensed under LICENSE_RMR.
+ *
+ * This file is part of the RMR module.
+ * It does not modify or replace the BLAKE3 core.
+ */
+
 #ifndef PAI_SIGN_H
 #define PAI_SIGN_H
 
