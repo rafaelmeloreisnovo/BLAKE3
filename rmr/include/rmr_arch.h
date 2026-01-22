0e3ba03bd90a897291707f2e3d2d18b2f5b99172
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-01-22 18:40:04 -0300

    Update rmr_arch.h

--- rmr/include/rmr_arch.h
+++ rmr/include/rmr_arch.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024–2026 Rafael Melo Reis
+ * Licensed under LICENSE_RMR.
+ *
+ * This file is part of the RMR module.
+ * It does not modify or replace the BLAKE3 core.
+ */
 #ifndef RMR_ARCH_H
 #define RMR_ARCH_H
 
